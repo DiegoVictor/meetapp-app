55bcc62301e176454f807a52045a8bf475030d0f
DiegoVictor <diegovictorgonzaga@gmail.com>
2019-09-12 21:46:10 -0300

    Create default layout

---
+++ src/components/layouts/Default/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Container, Header } from './styles';
+import Logo from '../../../assets/logo.svg';
+
+export default function Default({ children }) {
+  const user = useSelector(state => state.user);
+
+  return (
+    <Container>
+      {user && (
+        <Header>
+          <div>
+            <Link to="/dashboard">
+              <img src={Logo} alt="Meetapp" />
+            </Link>
+
+            <aside>
+              <div>
+                {/* <strong>{user.name}</strong> */}
+                <strong>Diego Victor</strong>
+                <Link to="/profile">Meu Perfil</Link>
+              </div>
+              <button type="button" onClick={() => {}}>
+                Sair
+              </button>
+            </aside>
+          </div>
+        </Header>
+      )}
+      {children}
+    </Container>
+  );
+}
+
+Default.propTypes = {
+  children: PropTypes.element.isRequired,
+};
---
+++ src/components/layouts/Default/styles.js
@@ -0,0 +1,50 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background: linear-gradient(180deg, #22202c, #402845);
+  height: 100vh;
+`;
+
+export const Header = styled.div`
+  background-color: rgba(0, 0, 0, 0.3);
+
+  > div {
+    align-items: center;
+    display: flex;
+    height: 64px;
+    justify-content: space-between;
+    margin: auto;
+    max-width: 900px;
+    width: 100%;
+
+    aside {
+      display: flex;
+      align-items: center;
+
+      div {
+        strong {
+          color: #fff;
+          display: block;
+          font-size: 14px;
+          line-height: 16px;
+          text-align: right;
+        }
+
+        a {
+          color: #999;
+          display: block;
+          font-size: 12px;
+          line-height: 16px;
+          margin-top: 2px;
+          text-align: right;
+        }
+      }
+
+      button {
+        display: inline-block;
+        margin-left: 30px;
+        margin-top: 2px;
+      }
+    }
+  }
+`;
