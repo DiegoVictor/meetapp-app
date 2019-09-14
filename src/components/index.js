478ad305a3e019aaf662a13d954945609c26a75a
DiegoVictor <diegovictorgonzaga@gmail.com>
2019-09-14 17:11:05 -0300

    Configure main entry point

--- src/components/index.js
+++ src/components/index.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import '../config/ReactotronConfig';
-import { StatusBar, YellowBox } from 'react-native';
+import { StatusBar } from 'react-native';
 import { Provider } from 'react-redux';
 
 import store from '../store';
 
-YellowBox.ignoreWarnings([
-  'Warning: Async Storage has been extracted from react-native core',
-  'Remote debugger is in a background tab which may cause apps to perform slowly. Fix this by foregrounding the tab (or opening it in a separate window).',
-]);
-
 export default function App() {
   return (
     <Provider store={store}>
-      <StatusBar barStyle="light-content" backgroundColor="#22202C" />
+      <StatusBar barStyle="light-content" backgroundColor="#191620" />
     </Provider>
   );
 }
