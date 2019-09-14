bfae74830a7a61a17e76608c0eb899bbeb3fd6ff
DiegoVictor <diegovictorgonzaga@gmail.com>
2019-09-14 00:01:19 -0300

    Set textarea style

--- src/components/pages/styles.js
+++ src/components/pages/styles.js
@@ -56,17 +56,23 @@ export default createGlobalStyle`
     }
   }
 
-  input {
+  input, textarea {
     background-color: rgba(0, 0, 0, 0.2);
     border: 0px;
     border-radius: 4px;
     color: rgba(255, 255, 255, 0.5);
+    font-family: Roboto, sans-serif;
     font-size: 18px;
     line-height: 18px;
     height: 50px;
     margin-bottom: 10px;
     padding: 14px 20px;
     width: 100%;
+  }
+
+  textarea {
+    height: auto;
+    resize: none;
   }
 
   h2 {
