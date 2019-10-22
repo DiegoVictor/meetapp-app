f93bc8dfc247983cef6fbf3f561014f7466d89ac
DiegoVictor <diegovictorgonzaga@gmail.com>
2019-10-21 22:09:11 -0300

    Test sign in and up

---
+++ src/__tests__/components/pages/SignIn.test.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { MemoryRouter, Router } from 'react-router-dom';
+import { fireEvent, render, wait } from '@testing-library/react';
+import faker from 'faker';
+
+import { signInRequest } from '~/store/actions/user';
+import history from '~/services/history';
+import SignIn from '~/components/pages/Sign/In';
+
+jest.mock('react-redux');
+
+describe('SignIn page', () => {
+  it('should be able to login', async () => {
+    const email = faker.internet.email();
+    const password = faker.internet.password();
+    const dispatch = jest.fn();
+
+    useDispatch.mockReturnValue(dispatch);
+
+    const { getByPlaceholderText, getByText } = render(
+      <MemoryRouter>
+        <SignIn />
+      </MemoryRouter>
+    );
+
+    fireEvent.change(getByPlaceholderText('Digite seu e-mail'), {
+      target: { value: email },
+    });
+    fireEvent.change(getByPlaceholderText('Sua senha secreta'), {
+      target: { value: password },
+    });
+    fireEvent.click(getByText('Entrar'));
+
+    await wait(() => {
+      expect(dispatch).toHaveBeenCalledWith(signInRequest(email, password));
+    });
+  });
+
+  it('should be able to click on signup link', async () => {
+    const { getByText } = render(
+      <Router history={history}>
+        <SignIn />
+      </Router>
+    );
+
+    fireEvent.click(getByText('Criar conta gratuita'));
+    expect(history.location.pathname).toBe('/signup');
+  });
+});
---
+++ src/__tests__/components/pages/SignUp.test.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { MemoryRouter, Router } from 'react-router-dom';
+import { fireEvent, render, wait } from '@testing-library/react';
+import faker from 'faker';
+
+import { signUpRequest } from '~/store/actions/user';
+import history from '~/services/history';
+import SignUp from '~/components/pages/Sign/Up';
+
+jest.mock('react-redux');
+
+describe('SignUp page', () => {
+  it('should be able to register', async () => {
+    const name = faker.name.findName();
+    const email = faker.internet.email();
+    const password = faker.internet.password();
+    const dispatch = jest.fn();
+
+    useDispatch.mockReturnValue(dispatch);
+
+    const { getByPlaceholderText, getByText } = render(
+      <MemoryRouter>
+        <SignUp />
+      </MemoryRouter>
+    );
+
+    fireEvent.change(getByPlaceholderText('Nome completo'), {
+      target: { value: name },
+    });
+    fireEvent.change(getByPlaceholderText('Digite seu e-mail'), {
+      target: { value: email },
+    });
+    fireEvent.change(getByPlaceholderText('Sua senha secreta'), {
+      target: { value: password },
+    });
+    fireEvent.click(getByText('Cadastrar'));
+
+    await wait(() => {
+      expect(dispatch).toHaveBeenCalledWith(
+        signUpRequest(email, name, password)
+      );
+    });
+  });
+
+  it('should be able to click on signin link', async () => {
+    const { getByText } = render(
+      <Router history={history}>
+        <SignUp />
+      </Router>
+    );
+
+    fireEvent.click(getByText('Já tenho conta'));
+    expect(history.location.pathname).toBe('/');
+  });
+});
