5fceb60282b7f4d1a0996f921766fd4f5851e173
DiegoVictor <diegovictorgonzaga@gmail.com>
2019-12-07 21:03:28 -0300

    Fix #1 Test Edit and Create pages

--- src/components/pages/Edit/index.js
+++ src/components/pages/Edit/index.js
@@ -1,45 +1,62 @@
-import React, { useEffect, useState } from 'react';
-import { format, parseISO } from 'date-fns';
+import React, { useCallback, useEffect, useState } from 'react';
+import { parseISO } from 'date-fns';
 import { MdArrowBack, MdCameraAlt, MdSave } from 'react-icons/md';
 import { useDispatch } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
 import PropTypes from 'prop-types';
 
+import Datepicker from '~/components/Datepicker';
 import { upsertMeetup } from '~/store/actions/meetup';
 import { Container, ImagePicker } from './styles';
 import api from '~/services/api';
 
-const schema = Yup.object().shape({
-  description: Yup.string()
-    .min(10, 'Poxa, explica mais sobre esse meetup ;)')
-    .required('O campo descrição é obrigatório'),
-  localization: Yup.string().required('O campo localização é obrigatório'),
-  title: Yup.string()
-    .min(6, 'O título deve ter no minimo 6 caracteres')
-    .required('O campo título é obrigatório'),
-});
-
 export default function Edit({ match, history }) {
-  const [banner_id, setBannerId] = useState('');
-  const [date, setDate] = useState('');
   const dispatch = useDispatch();
   const { id } = match.params;
   const [meetup, setMeetup] = useState({});
   const [preview, setPreview] = useState(null);
-  const [time, setTime] = useState('');
+
+  const uploadBanner = useCallback(
+    e => {
+      (async () => {
+        const data = new FormData();
+        data.append('file', e.target.files[0]);
+        const response = await api.post('files', data);
+
+        setMeetup({
+          ...meetup,
+          banner_id: response.data.id,
+        });
+        setPreview(response.data.url);
+      })();
+    },
+    [meetup]
+  );
+
+  const schema = Yup.object().shape({
+    banner: id
+      ? Yup.string()
+      : Yup.string().required('O campo banner é obrigatório'),
+    date: Yup.date().required('O campo data é obrigatório'),
+    description: Yup.string()
+      .min(10, 'Poxa, explica mais sobre esse meetup ;)')
+      .required('O campo descrição é obrigatório'),
+    localization: Yup.string().required('O campo localização é obrigatório'),
+    title: Yup.string()
+      .min(6, 'O título deve ter no minimo 6 caracteres')
+      .required('O campo título é obrigatório'),
+  });
 
   useEffect(() => {
     (async () => {
       if (id) {
         const { data } = await api.get(`scheduled/${id}`);
-        const dt = parseISO(data.date);
-
-        setBannerId(data.banner_id);
-        setDate(format(dt, "yyyy'-'MM'-'dd"));
-        setMeetup(data);
+        setMeetup({
+          ...data,
+          date: parseISO(data.date),
+        });
         setPreview(data.banner.url);
-        setTime(format(dt, "HH':'mm"));
       }
     })();
   }, [id]);
@@ -48,6 +65,7 @@ export default function Edit({ match, history }) {
     <Container>
       <div>
         <button
+          data-testid="back"
           type="button"
           className="unstyled"
           onClick={() => history.goBack()}
@@ -55,59 +73,56 @@ export default function Edit({ match, history }) {
           <MdArrowBack color="#FFF" size="24" />
         </button>
       </div>
+
       <Form
         initialData={meetup}
         schema={schema}
         onSubmit={data => {
           dispatch(
             upsertMeetup({
+              ...meetup,
               ...data,
               id,
-              banner_id,
-              date: `${date}T${time}:00-03:00`,
             })
           );
         }}
       >
         <ImagePicker htmlFor="banner">
-          {preview && <img src={preview} alt="" />}
+          {preview && <img src={preview} alt="Preview" />}
           <div>
             <MdCameraAlt size="54" />
             <span>Selecionar imagem</span>
           </div>
-          <input
+          <Input
+            data-testid="file"
+            accept="image/*"
             id="banner"
+            name="banner"
             type="file"
-            onChange={async e => {
-              const data = new FormData();
-              data.append('file', e.target.files[0]);
-              const response = await api.post('files', data);
-
-              setBannerId(response.data.id);
-              setPreview(response.data.url);
-            }}
+            onChange={uploadBanner}
           />
         </ImagePicker>
         <Input name="title" type="text" placeholder="Título do Meetup" />
         <Input
           name="description"
           multiline
-          rows="10"
           placeholder="Descrição completa"
+          rows={10}
           value={meetup.description}
           onChange={e => setMeetup({ ...meetup, description: e.target.value })}
         />
-        <input
-          type="date"
-          value={date}
-          onChange={e => setDate(e.target.value)}
+        <Datepicker
+          name="date"
+          placeholderText="Data do evento"
+          value={meetup.date}
+          onChange={value => {
+            setMeetup({
+              ...meetup,
+              date: value,
+            });
+          }}
         />
-        <input
-          type="time"
-          value={time}
-          onChange={e => setTime(e.target.value)}
-        />
-        <Input name="localization" type="text" placeholder="Localização" />
+        <Input name="localization" placeholder="Localização" type="text" />
         <div>
           <button type="submit">
             <MdSave size="17" />
