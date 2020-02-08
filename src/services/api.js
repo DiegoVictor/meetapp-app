2581373227dcea522046e6e49b9dec4c5b26ce2f
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-02-08 16:33:35 -0300

    Read env variable

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
+import Constants from 'expo-constants';
+
+const { API_URL } = Constants.manifest.extra;
 
 export default axios.create({
-  baseURL: 'https://meetapp-api.herokuapp.com',
+  baseURL: API_URL,
 });
