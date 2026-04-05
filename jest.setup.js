fb498ba1f76c5789eaf5c3f81000d5e36d68bb95
Diego Victor <diegovictorgonzaga@gmail.com>
2026-04-04 22:11:48 -0300

    Add Jest mock for react-native-gesture-handler to improve testing setup

---
+++ jest.setup.js
@@ -0,0 +1,33 @@
+jest.mock('react-native-gesture-handler', () => {
+  const { View, TouchableOpacity } = require('react-native');
+
+  return {
+    Swipeable: View,
+    DrawerLayout: View,
+    State: {},
+    ScrollView: View,
+    Slider: View,
+    Switch: View,
+    TextInput: View,
+    ToolbarAndroid: View,
+    ViewPagerAndroid: View,
+    DrawerLayoutAndroid: View,
+    WebView: View,
+    NativeViewGestureHandler: View,
+    TapGestureHandler: View,
+    FlingGestureHandler: View,
+    ForceTouchGestureHandler: View,
+    LongPressGestureHandler: View,
+    PanGestureHandler: View,
+    PinchGestureHandler: View,
+    RotationGestureHandler: View,
+    RawButton: View,
+    BaseButton: View,
+    RectButton: View,
+    BorderlessButton: View,
+    FlatList: View,
+    gestureHandlerRootHOC: jest.fn(),
+    Directions: {},
+    TouchableOpacity: TouchableOpacity,
+  };
+});
