00301a9fc6826698f4e7ba1e54adae1e127cd933
Diego Victor <diegovictorgonzaga@gmail.com>
2026-04-10 08:53:40 -0300

    Add main application entry point with Redux and routing setup

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import Container from '~/components/Container';
+import Routes from '~/routes';
+import store from '~/store';
+
+export default () => {
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistStore(store)}>
+        <StatusBar barStyle="light-content" backgroundColor="#191620" />
+        <Container>
+          <Routes />
+        </Container>
+      </PersistGate>
+    </Provider>
+  );
+};
