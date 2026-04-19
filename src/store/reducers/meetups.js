e7a5b659b6f0a38d8353b07a243720546066b00d
Diego Victor <diegovictorgonzaga@gmail.com>
2026-04-19 10:24:08 -0300

    Refactor meetups reducer to correct initial state variable and update unsubscribe logic

--- src/store/reducers/meetups.js
+++ src/store/reducers/meetups.js
@@ -1,8 +1,8 @@
 import { produce } from 'immer';
 
-const initial_state = [];
+export const initialState = [];
 
-export default (state = initial_state, action) => {
+export default (state = initialState, action) => {
   switch (action.type) {
     case '@meetup/APPEND_MEETUPS':
       return [...state, ...action.payload];
@@ -10,17 +10,10 @@ export default (state = initial_state, action) => {
     case '@meetup/SET_MEETUPS':
       return action.payload;
 
-    case '@meetup/SUBSCRIBE_MEETUP_SUCCESS':
+    case '@meetup/UNSUBSCRIBE_MEETUP_SUCCESS':
       return produce(state, draft => {
         const index = draft.findIndex(m => m.id === action.payload.id);
         draft.splice(index, 1);
-      });
-
-    case '@meetup/UNSUBSCRIBE_MEETUP_SUCCESS':
-      return produce(state, draft => {
-        draft.push(action.payload);
-        draft.sort((a, b) => (a.title < b.title ? -1 : 1));
-        draft.sort((a, b) => new Date(a.date) - new Date(b.date));
       });
 
     default:
