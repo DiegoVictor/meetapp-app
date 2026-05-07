3f595646578f3323ef780ad6f427be672ea4fadb
Diego Victor <diegovictorgonzaga@gmail.com>
2026-05-07 20:18:52 -0300

    Simplify babel configuration by removing unnecessary presets and plugins

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,3 @@
 module.exports = {
-  presets: [
-    'module:metro-react-native-babel-preset',
-    'module:react-native-dotenv',
-  ],
-  plugins: [
-    [
-      'babel-plugin-root-import',
-      {
-        rootPathSuffix: 'src',
-      },
-    ],
-  ],
+  presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
 };
