d090cfb8dd40529462f84ca9df5bf49f97614d3c
Diego Victor <diegovictorgonzaga@gmail.com>
2026-05-08 17:15:56 -0300

    Simplify routing structure by replacing react-navigation with NavigationContainer

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,93 +1,21 @@
-import React from 'react';
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
-import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { useSelector } from 'react-redux';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import { NavigationContainer } from '@react-navigation/native';
+import { PublicRoutes } from './public.routes';
+import { PrivateRoutes } from './private.routes';
 
-import { setNavigator } from '~/services/navigator';
-import { Dashboard } from '~/pages/Dashboard';
-import { Profile } from '~/pages/Profile';
-import { SignIn } from '~/pages/Sign/In';
-import { SignUp } from '~/pages/Sign/Up';
-import { Subscription } from '~/pages/Subscription';
+const ref = createNavigationContainerRef();
+export function navigate(name, params) {
+  if (ref.isReady()) {
+    ref.navigate(name, params);
+  }
+}
 
-export const Routes = () => {
-  const signed = useSelector(state => state.signed);
-  const Router = createAppContainer(
-    createSwitchNavigator(
-      {
-        Sign: createSwitchNavigator({
-          SignIn,
-          SignUp,
-        }),
-        App: createBottomTabNavigator(
-          {
-            Dashboard: {
-              screen: Dashboard,
-              navigationOptions: {
-                tabBarLabel: 'Meetups',
-                tabBarIcon: ({ tintColor }) => (
-                  <Icon
-                    name="format-list-bulleted"
-                    size={20}
-                    color={tintColor}
-                  />
-                ),
-              },
-            },
-            Profile: {
-              screen: Profile,
-              navigationOptions: {
-                tabBarLabel: 'Meu perfil',
-                tabBarIcon: ({ tintColor }) => (
-                  <Icon name="person" size={20} color={tintColor} />
-                ),
-              },
-            },
-            Subscription: {
-              screen: Subscription,
-              navigationOptions: {
-                tabBarLabel: 'Inscrições',
-                tabBarIcon: ({ tintColor }) => (
-                  <Icon name="local-offer" size={20} color={tintColor} />
-                ),
-              },
-            },
-          },
-          {
-            tabBarOptions: {
-              keyboardHidesTabBar: true,
-              activeTintColor: '#FFF',
-              inactiveTintColor: 'rgba(255, 255, 255, 0.6)',
-              style: {
-                backgroundColor: '#2B1A2F',
-                border: 0,
-                borderTopColor: '#2B1A2F',
-                fontSize: 12,
-                lineHeight: 14,
-                paddingBottom: 5,
-                paddingTop: 5,
-              },
-            },
-          }
-        ),
-      },
-      {
-        initialRouteName: (() => {
-          if (signed) {
-            return 'App';
-          }
-          return 'Sign';
-        })(),
-      }
-    )
-  );
+export const AppRoutes = () => {
+  const signed = useSelector((state) => state.signed);
 
   return (
-    <Router
-      ref={nav => {
-        setNavigator(nav);
-      }}
-    />
+    <NavigationContainer ref={ref}>
+      {signed ? <PrivateRoutes /> : <PublicRoutes />}
+    </NavigationContainer>
   );
 };
