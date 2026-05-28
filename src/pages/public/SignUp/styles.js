4e4c629b05d79c31195bbad00afd889be1af4056
Diego Victor <diegovictorgonzaga@gmail.com>
2026-05-28 08:34:27 -0300

    Add SignUp page with form and navigation links

---
+++ src/pages/public/SignUp/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+import { TextButton } from '../../../components/Button/styles';
+
+export const Form = styled.View`
+  justify-content: center;
+  margin-top: 50px;
+  max-width: 315px;
+  text-align: center;
+  width: 100%;
+`;
+
+export const TextLink = styled(TextButton)`
+  margin-top: 20px;
+`;
