a87771cb60bcdc8df30a82e4fcf1b2e63d7cff17
Diego Victor <diegovictorgonzaga@gmail.com>
2026-06-04 11:43:15 -0300

    Change Container component to use styled.View instead of styled.SafeAreaView

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.SafeAreaView`
+export const Container = styled.View`
   align-items: center;
   flex: 1;
   justify-content: center;
