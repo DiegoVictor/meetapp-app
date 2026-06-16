fc027c5e9842c7d1e1b0ff7596f868fa02b1a3d8
Diego Victor <diegovictorgonzaga@gmail.com>
2026-06-16 09:25:14 -0300

    Refactor SignUp tests to improve mock usage and enhance input handling

--- tests/integration/pages/SignUp.spec.js
+++ tests/integration/pages/SignUp.spec.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import { fireEvent, render } from '@testing-library/react-native';
-import { useDispatch } from 'react-redux';
+import React, { act } from 'react';
+import {
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+} from '@testing-library/react-native';
 import { signUpRequest } from '../../../src/store/actions/user';
 import { SignUp } from '../../../src/pages/public/SignUp';
 import { factory } from '../../utils/factory';
 
-jest.mock('react-redux');
+const mockDispatch = jest.fn();
+jest.mock('react-redux', () => {
+  return {
+    useDispatch: () => mockDispatch,
+  };
+});
 
 const mockNavigate = jest.fn((args) => args);
 jest.mock('@react-navigation/native', () => {
   return {
     useNavigation: () => ({
-      navigate: mockNavigate(),
+      navigate: (args) => mockNavigate(args),
     }),
   };
 });
@@ -19,8 +28,7 @@ jest.mock('@react-navigation/native', () => {
 jest.mock('../../../src/components/Input', () => {
   const { TextInput } = require('react-native');
   return {
-    __esModule: true,
-    default: TextInput,
+    Input: TextInput,
   };
 });
 
@@ -28,52 +36,54 @@ describe('SignUp', () => {
   it('should be able to sign up', async () => {
     const { name, email, password } = await factory.attrs('User');
 
-    const dispatch = jest.fn();
-    useDispatch.mockReturnValue(dispatch);
-
     const { getByPlaceholderText, getByTestId } = await render(<SignUp />);
 
-    fireEvent.changeText(getByPlaceholderText('Nome completo'), name);
-    fireEvent.changeText(getByPlaceholderText('Digite seu email'), email);
-    fireEvent.changeText(getByPlaceholderText('Sua senha secreta'), password);
+    act(() => {
+      fireEvent.changeText(getByPlaceholderText('Nome completo'), name);
+      fireEvent.changeText(getByPlaceholderText('Digite seu email'), email);
+      fireEvent.changeText(getByPlaceholderText('Sua senha secreta'), password);
+    });
 
     fireEvent.press(getByTestId('submit'));
 
-    expect(dispatch).toHaveBeenCalledWith(signUpRequest(email, name, password));
+    expect(mockDispatch).toHaveBeenCalledWith(
+      signUpRequest(email, name, password)
+    );
   });
 
   it('should be able to sign up changing input focus and submitting from keyboard', async () => {
     const { name, email, password } = await factory.attrs('User');
 
-    const dispatch = jest.fn();
-    useDispatch.mockReturnValue(dispatch);
-
-    const { getByPlaceholderText } = await render(<SignUp />);
+    const { getByPlaceholderText, getByTestId } = await render(<SignUp />);
 
     const nameInput = getByPlaceholderText('Nome completo');
-    fireEvent.changeText(nameInput, name);
-    fireEvent(nameInput, 'onSubmitEditing');
-
     const emailInput = getByPlaceholderText('Digite seu email');
-    fireEvent.changeText(emailInput, email);
-    fireEvent(emailInput, 'onSubmitEditing');
-
     const passwordInput = getByPlaceholderText('Sua senha secreta');
-    fireEvent.changeText(passwordInput, password);
-    fireEvent(passwordInput, 'onSubmitEditing');
 
-    expect(dispatch).toHaveBeenCalledWith(signUpRequest(email, name, password));
+    act(() => {
+      fireEvent.changeText(nameInput, name);
+      fireEvent(nameInput, 'submitEditing');
+
+      expect(emailInput).toHaveAccessibilityValue({ selected: true });
+      fireEvent.changeText(emailInput, email);
+      fireEvent(emailInput, 'submitEditing');
+
+      expect(passwordInput).toHaveAccessibilityValue({ selected: true });
+      fireEvent.changeText(passwordInput, password);
+    });
+
+    fireEvent(passwordInput, 'submitEditing');
+
+    expect(mockDispatch).toHaveBeenCalledWith(
+      signUpRequest(email, name, password)
+    );
   });
 
   it('should be able to navigate to sign in screen', async () => {
-    const dispatch = jest.fn();
-    const navigator = { dispatch };
-
     const { getByTestId } = await render(<SignUp />);
 
     fireEvent.press(getByTestId('signin'));
 
     expect(mockNavigate).toHaveBeenCalledWith('SignIn');
-    expect(dispatch).not.toHaveBeenCalled();
   });
 });
