72cfe7911dc09b761b9d128be14b8fbb21ff0e70
Diego Victor <diegovictorgonzaga@gmail.com>
2026-06-22 18:58:03 -0300

    Remove React plugin configuration from ESLint setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,7 @@
 import js from "@eslint/js";
 import globals from "globals";
-import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
-  pluginReact.configs.flat.recommended,
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
 ]);
